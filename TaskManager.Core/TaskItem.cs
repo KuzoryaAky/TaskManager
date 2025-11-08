6aeab77c6fa904baae06d59706e828eba1725c59
KuzoryaAky <opanforever1992@gmail.com>
2025-11-08 10:02:23 +0300

    setup project dependencies and entities

---
+++ TaskManager.Core/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Core
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public int Priority { get; set; } = 1;
+    }
+}
