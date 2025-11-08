acccf4a3f427e2e42c3c4bc58503f690d3d5db04
KuzoryaAky <opanforever1992@gmail.com>
2025-11-08 17:14:53 +0300

    add React to project

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManager.Infrastructure.Data;
+using TaskManager.Infrastructure.Repositiries;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<TaskRepository>();
+
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>   
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,9 +32,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthentication();
+app.UseCors();
 app.MapControllers();
+app.UseStaticFiles();
 
 app.Run();
 
