6dd5353515ac71847ace28540994f076b068f44c
KuzoryaAky <opanforever1992@gmail.com>
2025-11-08 17:14:08 +0300

    update Core

--- TaskManager.Core/TaskItem.cs
+++ TaskManager.Core/TaskItem.cs
@@ -1,13 +1,27 @@
-﻿namespace TaskManager.Core.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace TaskManager.Core.Entities
 {
     public class TaskItem
     {
         public int Id { get; set; }
+
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+
+        [JsonPropertyName("description")]
         public string Description { get; set; } = string.Empty;
+
+        [JsonPropertyName("createdAt")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [JsonPropertyName("dueDate")]
         public DateTime? DueDate { get; set; }
+
+        [JsonPropertyName("isCompleted")]
         public bool IsCompleted { get; set; }
+
+        [JsonPropertyName("priority")]
         public int Priority { get; set; } = 1;
     }
 }
